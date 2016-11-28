68dc902741fb0c936c88c39515fd923584a5a33b
yicheng0922 <yicheng0922@hotmail.com>
2016-11-28 00:16:53 -0500

    created shape drawing function for visualization

---
+++ matlab/draw_box.m
@@ -0,0 +1,19 @@
+function [ output_args ] = draw_box( shape, S )
+% this function draws a box based on the shape data input
+% S is the same character string used for plot(). Please see help plot for
+% detail explanation
+
+% make sure the shape data is a box    
+assert(shape.isRect);
+
+% compute the box vertex
+vertex = compute_box_vertices( shape );
+
+vertex = [vertex vertex(:,1)];
+
+% plot the box
+plot(vertex(1,:),vertex(2,:), S);
+
+
+end
+
---
+++ matlab/draw_fan.m
@@ -0,0 +1,19 @@
+function [ output_args ] = draw_fan( shape, S )
+% this function draw the fan shape based on the shape data provided
+% S is the same character string used for plot(). Please see help plot for
+% detail explanation
+
+% make sure the shape is a fan
+assert(~shape.isRect);
+
+% splitting the arc into 100 points
+t = linspace(shape.alpha1,shape.alpha2);
+xx = shape.x + shape.radius*cos(t);
+yy = shape.y + shape.radius*sin(t);
+
+% plot the shape
+plot([shape.x,xx,shape.x],[shape.y,yy,shape.y],S);
+
+
+end
+
